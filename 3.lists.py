ac695d8c58088fb6edf294063b8f1ab6d5ddef0a
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2023-12-11 12:07:32 +0530

    Sorting

--- 3.lists.py
+++ 3.lists.py
@@ -15,8 +15,24 @@ fruits.extend(['cherry','orange'])  #-->to extend list
 fruits[3]="Kiwi"        #-->to replace
 fruits[1:2]=['strawberry','banana']  #-->to change multiple items  ([1:3] --> lessthan one value)
 
-fruits=['Apple', 'strawberry', 'banana', 'Mango', 'Kiwi', 'cherry', 'orange','Watermelon']  # To remove items
+  # To remove items
+fruits=['Apple', 'strawberry', 'banana', 'Mango', 'Kiwi', 'cherry', 'orange','Watermelon']
 fruits.remove('cherry')
-fruits.pop(2)  #-->a particular item
+fruits.pop(2)    #-->a particular item =use index
 del fruits[3]
+fruits.clear()  #to clear all list
 print(fruits)
+
+#Sorting
+names = ['abhi','anil','chitra',"pranavi",'potti',"pavan","Sujith","Nishanth","Lavanya"]
+names[6:9]=['sujith','nishanth','lavanya']  #change capital to small
+names.sort()  #->it takes capital letter first
+#names.reverse()  #reverse order
+names.sort(reverse = True)  #Descending Order
+
+print(names)
+
+
+n = [47,7,4,11,14,24,2,25,65,76,54]
+n.sort(reverse= True)
+print(n)
