a3f8bd6268680dfd3e30f0ec24530e67f2857a9f
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2023-12-13 15:55:38 +0530

    length

--- 5.sets_dictionaries.py
+++ 5.sets_dictionaries.py
@@ -2,3 +2,10 @@
 
 flowers = {'Lilly','Rose','Jasmine','Lotus'}
 print(flowers)
+
+# Diff Data types
+n = {"Pranavi",4,True,"Lavanya"}
+set = {"Pranavi",4,True,"Pavan",4}  # Don't allow duplicate values
+print(set)
+print(len(set))
+print(len(n))
