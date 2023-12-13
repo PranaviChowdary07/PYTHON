ddf83c20302706a5d03dc9c4f632f21886a58063
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2023-12-13 16:31:03 +0530

    Remove

--- 5.sets_dictionaries.py
+++ 5.sets_dictionaries.py
@@ -12,5 +12,22 @@ print(len(n))
 
         # Dictionary
 person = {"name":"Pranavi","age":21,"mobile":'741411'}
-print(person['name'])         # How to cal values
+print(person['name'])                # How to cal values
+person ["mobile"] = 74141123         # Change values
+person ["frnd"] = "Lavanya"
+            # update,Add
+person.update({"name":"Sujith"}) 
+person.update({"mail":"pranavi143@gmail.com"}) 
+    # Remove methods
+person.pop("mail")  
+person.popitem()  # without key remove value but it remove last value
 print(person)
+
+person1 = {'name': 'Sujith', 'age': 21,"lastname":"Kumar", 'mobile': 74141123, 'frnd': 'Lavanya', 'mail': 'pranavi143@gmail.com'}
+del person1 ["lastname"]   #use del keyword
+# del person1  -->it completly delete when print it shows person1 is not defined
+print(person1)
+
+p ={"name":"Pranavi","Hero":"Ram"}
+p.clear() # no delete but do empty
+print(p)
