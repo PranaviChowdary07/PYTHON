2005eb326189a4d449ef58247b018d1ef19c735a
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2023-12-17 11:56:22 +0530

    List Programs

--- 4.lists.py
+++ 4.lists.py
@@ -42,3 +42,22 @@ print(n)
 p =[34,64,14,11,25,2,7,4,10,27,23,28]
 print(min(p))
 print(max(p))
+
+""" Example Programs """
+
+#Write a program to count the no.of in specified list within a range
+
+n = [1,3,5,6,8,4,9,10.45,76,78,45,6]
+lower_bound = 4
+upper_bound = 76
+count_within_range = len([num for num in n if lower_bound <= num <=upper_bound])
+print(n)
+print(count_within_range)
+
+# Write a program to create list by concatenating given list with a range from 1 to n.
+# Sample input :['p','q'],n=5,expected output = ['p1','q1','p2','q2','p3','q3','p4','q4','p5','q5']
+
+a =[ 'p','q']
+n = 5
+result = [item + str(i) for i in range(1,n+1) for item in a]
+print(result)
