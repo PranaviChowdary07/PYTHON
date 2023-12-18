50cbc81563c5b139fa42b79f47dee1869317353d
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2023-12-18 16:38:42 +0530

    break & continue

--- 8.loops.py
+++ 8.loops.py
@@ -9,3 +9,32 @@ while a < 10:
     print(a,"Learning Python")
 
 # For Loop
+for  x  in range (5):      # how many values print
+    print(x)
+
+for a  in range(1,10):      # where to start and end 
+    print(a)
+
+
+for p in range (1,100,10):   # add value to p
+    print(p)
+
+# Example
+name = ['Pranavi','Sujith','Pavan','Potti','Lavanya','Nishanth']    # print list,tuple,set
+for i in name:
+    print(i)
+
+# print characters
+name = "Pranavi"
+for i in name:
+    print(i)
+
+
+# use break,and continue
+people =  ['Pranavi','Sujith','Pavan','Potti','Lavanya','Nishanth']
+for i in people:
+  
+    if i == "Pavan":
+        #break
+          continue
+    print(i)
