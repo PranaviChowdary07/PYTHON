38f6d15bc85ff55e1e4b3eac04f17d4aaeb2d820
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-01-06 09:24:06 +0530

    user

---
+++ 9.userinput.py
@@ -0,0 +1,11 @@
+x = int(input("Enter the num1: "))
+y = int(input("Enter the num2: "))
+z = x +y
+print("Result: ",z)
+
+p = input("Enter any char: ")
+while len(p) != 1:
+    print("Please enter single char only\n")
+    p = input("Enter any char: ")
+    print(p)
+    
