b6e4730abe5ec55fab3dc831a317f2d2c925298d
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-01-10 20:13:34 +0530

    Data user

--- 11.keyword_args.py
+++ 11.keyword_args.py
@@ -8,3 +8,32 @@ def person(**data):
         else:
             print(k,':',v)
 person(fname = 'Pranavi',lname = 'Kasthuri',age = 21,mobile =9053456)
+
+# Getting data from user
+def person1(**data):
+    for k,v in data.items():
+        if k == 'firstname':     # edit spaces for equal length
+            print(k, '   :', v)
+        elif k =='lastname':
+            print(k,'    :',v)
+        elif k == 'age':
+            print(k,'         :', v)
+        else:
+            print(k,':',v)
+
+e = 1
+while e != '0':                                        # allow may user details
+    fname =input("Enter first name: ")
+    lname =input("Enter last name: ")
+    age =input("Enter age: ")
+    mobilenum= input("Enter mobile number: ")
+    print(' ')                                      # use for line break
+    person1(firstname=fname,lastname=lname,age=age,mobilenumber=mobilenum)
+    e = input("Enter 0 to exit or any key to continue: ")
+    print(' ')
+
+
+    # Sending Dictionary as an Argument
+    
+
+    
