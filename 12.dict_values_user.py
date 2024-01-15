7dd2e196da932ebbe2c8277d417224cf60b1a223
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-01-15 15:47:12 +0530

    Equal edit sapces

--- 12.dict_values_user.py
+++ 12.dict_values_user.py
@@ -1,16 +1,28 @@
 # Getting  keys & values from user
 def printPerson(data):
-    for k, v in data.items():
-        print(k, ":", v)
+    hkl = 0
+    for kl, vl in data.items():
+        if hkl < len(kl):
+            hkl = len(kl)
+    for k,v in data.items():
+        if hkl == len(k):
+            print(k, ":", v)
+        else:
+            space =''
+            balance_ln = hkl-len(k)
+            for xs in range(balance_ln):
+                space += ' '
+            print(k + space,':',v)
+
 
 userdata = {}
 e = 1
 while e != '0':
-    userkey = input("Enter {key} : ")
-    uservalue = input("Enter {value} : ")
+    userkey = input("Enter key : ")
+    uservalue = input("Enter value : ")
     userdata[userkey] = uservalue
     e = input("Enter 0 to end or any key to continue adding.....")
-    userdata += "{key: <10}: {value}\n"
+    
 printPerson(userdata)
     
     
