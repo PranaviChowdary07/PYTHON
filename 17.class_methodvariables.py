a16de70bff2b63d23928240b90a03c5753e986ab
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-01-20 13:20:45 +0530

    Instance Methods

--- 17.class_methodvariables.py
+++ 17.class_methodvariables.py
@@ -31,7 +31,8 @@ print(bike2.name,bike2.year,bike2.color,Bike.tyres)
                       #  Types of Methods in Class
 """ 
    1.Instance Methods
-
+      --> get method
+      --> set method
    2.Class Methods
 
    3.Static method
@@ -42,7 +43,41 @@ class Student :
         self.marks1 = 'Marks 1'
         self.marks2 = 'Marks 2'
         self.marks3 = 'Marks 3'
+        # Instance methods
+        # == GET Methods == Accessors(another name)
+    def get_m1(self):
+        return self.marks1
+    
+
+    def get_m2(self):
+        return self.marks2
+    
+
+    def get_m3(self):
+        return self.marks3
+      # == Set Method == Mutators
+    def set_m1(self,value):
+        self.marks1 = value
+    
+
+    def set_m2(self,value):
+        self.marks2 = value
+
+
+    def set_m3(self,value):
+        self.marks3 = value
+    
+    
+        
 
 S1 = Student()
 S2 = Student() 
+
+
+S1.set_m1(98)
+print(S1.get_m1())
+
+S2.set_m1(96)
+print(S2.get_m1())
+
 
