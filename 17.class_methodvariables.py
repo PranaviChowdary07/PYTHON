cfb28bafc2bf7bd0bfcd9905beda2726fa4a58b0
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-01-20 11:50:06 +0530

    Class Variables

---
+++ 17.class_methodvariables.py
@@ -0,0 +1,9 @@
+""" 
+ Class Variables are two types :-
+    1.Class or Static 
+      --> no change
+    2.Instance -->used in def function
+      --> it instnace changes to every obj
+ """
+
+# Example
