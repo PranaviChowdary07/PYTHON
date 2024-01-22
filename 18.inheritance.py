73f02c783187e37f9af17d045db212428932a33e
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-01-22 17:46:57 +0530

    Example

--- 18.inheritance.py
+++ 18.inheritance.py
@@ -1,1 +1,19 @@
-#
+""" 
+Inheritance :- class methods and properties used in another class .
+
+ -- Types :-
+
+        1.Single Inheritance
+           - One parent class,one child class
+
+ """
+
+ # Example 
+class A:   # Parent Class Or Base Class
+    x = "Something"
+
+class B(A):  # Child Class or Derived Class
+    y = "Nothing"
+
+obj = A()
+print(obj.x)
