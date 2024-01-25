748fbb3f7749a5ac5b4735a2b1d9d7b633e33ffe
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-01-25 11:01:18 +0530

    Multipl Inheritance

--- 18.inheritance.py
+++ 18.inheritance.py
@@ -5,6 +5,8 @@ Inheritance :- class methods and properties used in another class .
 
         1.Single Inheritance
            - One parent class,one child class
+        2.Multiple Inheritance
+           - Multiple parent classes ,child class(accept multiple parent class)
 
  """
 
@@ -33,3 +35,26 @@ class Child(Parent):
 obj = Child()
 obj.fun1() # --> O/P = Parent
 obj.fun2() # --> O/P = Child
+
+# Multiple Inheritace
+
+class Mother:
+    mother_name = ""
+    def mom(self):
+        print(self.mother_name)
+
+class Father:
+     father_name =""
+     def dad(self):
+         print(self.father_name)
+
+class Son(Mother,Father):
+    son_name = ""
+    def shownames(self):
+        print(self.son_name," Son of Mr & Mrs",self.father_name ,'-',self.mother_name)
+
+obj = Son()
+obj.father_name = "Arjun"
+obj.mother_name = "Sneha"
+obj.son_name = "Ayan"
+obj.shownames()
