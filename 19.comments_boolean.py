a890cc1a61053f0fcb882d3a77568ecfec14ac24
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-01-30 10:52:43 +0530

    Comments

---
+++ 19.comments_boolean.py
@@ -0,0 +1,12 @@
+ # print("Pranavi")    ---> Single line 
+     # for multiple lines once comment use --> ctrl + / create ,remove
+print("About comments & Booolean")
+print("About comments & Booolean")
+print("About comments & Booolean")
+print("About comments & Booolean")
+print("About comments & Booolean")
+
+# Another way
+""" 
+This is 3 line comments
+"""
