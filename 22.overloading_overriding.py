7d07a261af69f21b25918a8a6b4ab306e867ae68
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-02-02 10:26:40 +0530

    Behind code

--- 22.overloading_overriding.py
+++ 22.overloading_overriding.py
@@ -1,2 +1,12 @@
 """ Operator Overloading & Method Overriding """
 """ 8 + 4 --> 8,9 are operands,+ is operator """
+
+a = 14
+b = 4
+print(a + b)
+# behind code running
+print(int.__add__(a,b))
+# For string
+a = "Pranavi"
+b = "Kasthuri"
+print(str.__add__(a,b))
