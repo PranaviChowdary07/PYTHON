a4258a020d2919ec519cadc1d70735feff419645
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-02-02 10:41:23 +0530

    Overloading Ex.

--- 22.overloading_overriding.py
+++ 22.overloading_overriding.py
@@ -10,3 +10,20 @@ print(int.__add__(a,b))
 a = "Pranavi"
 b = "Kasthuri"
 print(str.__add__(a,b))
+
+# Example:
+
+class Student:
+    def __init__(self,m1,m2) :
+        self.m1 = m1
+        self.m2 = m2
+    def __add__(self,other):   # when use the error doesnot occur
+        m1 = self.m1 + other.m1
+        m2 = self.m2 + other.m2
+        return Student(m1,m2)   # It converts to object
+
+s1  = Student(23,45)
+s2  = Student(45,57) # It shows type error: unsupported type for +: "student"
+s3 = s1 + s2
+# s3 = Student(55 + 56 , 56 + 2)
+print(s3.m1 ,s3.m2)
